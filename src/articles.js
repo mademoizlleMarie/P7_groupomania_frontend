f70e53d4880d68b61ac6a4159ac976ca9300f9bc
sylvestre <sylvestre.marie@gmail.com>
2020-12-14 21:28:10 +0100

    article --> affichage des articles #8

--- src/articles.js
+++ src/articles.js
@@ -5,71 +5,88 @@ const API_URL = `${API._HOST + API._DIR }articles`;
 
 const token =  sessionStorage.getItem('token');
 
-Request.call({
-    headers : {
-        'Content-Type': 'application/json',
-        'Authorization': token
-    },
-    url : API_URL
-}).then((result)=>{
-    showContent(result);
-}).catch((result)=> {
+function request() {
+    Request.call({
+        headers : {
+            'Content-Type': 'application/json',
+            'Authorization': token
+        },
+        url : API_URL,
+        status : 200
+    }).then((result)=>{
+        showArticle(result);
+    }).catch((result)=> {
 
     });
+}
+request();
 
-
-/*var getGroupomania = new Promise((resolve, reject) => {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            resolve(JSON.parse(this.responseText));
-        } else if (this.readyState == XMLHttpRequest.DONE && this.status != 200) {
-            reject(this.responseText);
-        }
-    };
-    request.open("GET", API_URL );
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.setRequestHeader('Authorization', token);
-    request.send();
+var nouvel_article = document.getElementById('nouvel_article');
+nouvel_article.addEventListener('click',function () {
+    document.getElementById('bloc_nouvel_article').style.display = "block";
 });
-getGroupomania.then((result)=>{
-    showContent(result);
-});
-getGroupomania.catch((result)=> {
-
-});*/
 
 
 // affiche des teddies avec les informations
-function showContent(listeProduit) {
-
+function showArticle(Articles) {
     // création d'une carte pour chaque produit
     let elt = document.getElementById('card');
-    for (let produit of listeProduit) {
+    for (let article of Articles) {
 
         var card = document.createElement("div");
         card.setAttribute("class", "card");
 
         var img = document.createElement("img");
         img.setAttribute("class", "card-img-top");
-        img.setAttribute("src", produit.imageUrl);
+        img.setAttribute("src", article.imageUrl);
 
         var cardBody = document.createElement("div");
         cardBody.setAttribute("class", "card-body");
 
+        var contenu = document.createElement("div");
+        contenu.setAttribute("class", "card-contenu");
+
         var title = document.createElement("h5");
         title.setAttribute("class", "card-title");
-        title.innerHTML = "Adoptez " + produit.titre_article;
-        cardBody.append(title);
+        title.innerHTML = article.titre_article;
+        contenu.append(title);
 
         var textDescription = document.createElement("p");
         textDescription.setAttribute("class", "card-text");
-        textDescription.innerHTML = produit.corps_article;
-        cardBody.append(textDescription);
+        textDescription.innerHTML = article.corps_article;
+        contenu.append(textDescription);
+        cardBody.append(contenu);
+
+        /* for (let commentaire of Commentaires) {
+             var afficheCommentaire = document.createElement('div');
+             afficheCommentaire.setAttribute("class", "commentaire");
+             afficheCommentaire.innerHTML = commentaire.commentaire;
+             cardBody.append(afficheCommentaire);
+         }*/
+
+        var ajoutCommentaire = document.createElement("div");
+        ajoutCommentaire.setAttribute("class", "ajoutCommentaire");
+
+        var textCommentaire = document.createElement("textarea");
+        textCommentaire.setAttribute("id", "commentaire");
+        textCommentaire.setAttribute("rows", "2");
+        textCommentaire.setAttribute("cols", "50");
+        textCommentaire.setAttribute("placeholder", "ajouter un commentaire");
+        textDescription.innerHTML = article.corps_article;
+        ajoutCommentaire.append(textCommentaire)
+
+        var enregisterCommentaire = document.createElement("a");
+        enregisterCommentaire.setAttribute("class", "btn ");
+        enregisterCommentaire.setAttribute("id", "enregisterCommentaire");
+        enregisterCommentaire.innerHTML = "Ajouter";
+        ajoutCommentaire.append(enregisterCommentaire);
+        cardBody.append(ajoutCommentaire);
 
         card.append(img);
         card.append(cardBody);
         elt.append(card);
     }
 }
+
+
 
